d22b7c1df45c93052f806351d4280f9b8bbe7b2b
Life4Hook <105364621+Life4Hook@users.noreply.github.com>
2022-05-11 21:59:44 +0300

    Add files via upload

---
+++ Task1.php
@@ -0,0 +1,18 @@
+<?php
+function add_to_array($data, $a){
+    for ($val = 0; $val < count($data); $val++):
+        if (strpos($data[$val], "2") !== false):
+            $data[] = 0;
+            for ($new_val = count($data) - 1; $new_val > $val; $new_val--):
+                $data[$new_val] = $data[$new_val - 1];
+            endfor;
+            $val += 1;
+            $data[$val] = $a;
+        endif;
+    endfor;
+    return $data;
+}
+
+$arr = [1, 32, 55, 82, 1596, 22, 19561];
+
+print_r(add_to_array($arr, 'NEW'));
